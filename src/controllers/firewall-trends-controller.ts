54e78d3471ba42761e3b618086a8aa4692f936d5
anarehder <anapaula.rehder@gmail.com>
2024-03-19 18:37:38 +0000

    feat: diferenca entre meses input - service

--- src/controllers/firewall-trends-controller.ts
+++ src/controllers/firewall-trends-controller.ts
@@ -1,11 +1,12 @@
 import httpStatus from "http-status";
 import { Request, Response } from "express";
 import { getFirewallMonthlyTrendsService } from "@/services";
+import { FirewallTrendsInputParams } from "@/protocols";
 
 export async function getFirewallMonthlyTrendsController(req: Request, res: Response) {
-    console.log(req.body);
+    const { start, end, value } = req.body as FirewallTrendsInputParams;
     try{
-        const response = await getFirewallMonthlyTrendsService();
+        const response = await getFirewallMonthlyTrendsService(start, end, value);
         return res.status(httpStatus.OK).send(response);
     } catch(error) {
         return res.status(httpStatus.BAD_REQUEST).send(error);
--- src/services/firewall-trends-services.ts
+++ src/services/firewall-trends-services.ts
@@ -1,14 +1,18 @@
 import { FirewallTrendsBiggestValueOutputDBParams, FirewallTrendsBiggestValueOutputParams, HostListOutputDBParams, ItemsByHostOutputDBParams } from "@/protocols";
 import { getFirewallMonthlyTrendsRepository, getHostsByHostGroupsRepository } from "@/repositories";
 import { getItemsByHostRepository } from "@/repositories/items-repository";
+import moment from "moment";
 
-export async function getFirewallMonthlyTrendsService() {
+export async function getFirewallMonthlyTrendsService(start:string, end: string, value: string) {
     const firewallGroup = 516;
 
-    const start = '2023-12-01 00:00:00';
-    const end = '2024-01-01 00:00:00'; 
-    const value = 'value_avg';
+    //verificar se end é depois de start
+    const startFormatted = `${start}-01 00:00:00`;
+    const endFormatted = `${end}-01 00:00:00`;
 
+    //se for mais de 1 mês de diferença devo pegar todos;
+    const monthDiff = monthDifference(start, end);
+    console.log(monthDiff);
     const response: FirewallTrendsBiggestValueOutputParams[]  = [];
 
     const hostsList: HostListOutputDBParams[] | any = await getHostsByHostGroupsRepository(firewallGroup);
@@ -17,7 +21,7 @@ export async function getFirewallMonthlyTrendsService() {
         const itemsByHost: ItemsByHostOutputDBParams[] | any = await getItemsByHostRepository(hostsList[i]);
         for (let j = 0; j < itemsByHost.length; j++) {
             const item = itemsByHost[j].itemid;
-            const trendBiggestValues: FirewallTrendsBiggestValueOutputDBParams[] | any = await getFirewallMonthlyTrendsRepository(item, start, end, value);
+            const trendBiggestValues: FirewallTrendsBiggestValueOutputDBParams[] | any = await getFirewallMonthlyTrendsRepository(item, startFormatted, endFormatted, value);
             const formattedValues = {
                 hostid: hostsList[i].hostid,
                 hostName : hostsList[i].hostName,
@@ -29,3 +33,14 @@ export async function getFirewallMonthlyTrendsService() {
 
     return response;
 }
+
+function monthDifference(month1: string, month2: string) {
+    // Converter as strings para objetos Moment.js
+    const momentData1 = moment(month1, 'YYYY-MM').startOf('month');
+    const momentData2 = moment(month2, 'YYYY-MM').startOf('month');
+  
+    // Calcular a diferença em meses
+    const monthDiff = momentData2.diff(momentData1, 'months');
+  
+    return monthDiff;
+  }
